47bb279b1d6bdae3fde6d15d14f2f3b36f5b6ea3
arturfil <arturfil@hotmail.com>
2021-05-14 05:18:11 -0400

    added the categoryId as a useEffect dependency

--- src/screens/SingleProductScreen.tsx
+++ src/screens/SingleProductScreen.tsx
@@ -1,6 +1,6 @@
 import { Picker } from '@react-native-picker/picker'
 import { StackScreenProps } from '@react-navigation/stack'
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
 import GreenButton from '../components/GreenButton'
 import { ProductsContext } from '../context/ProductsContext'
@@ -12,7 +12,7 @@ import { formColor, loginStyles, selectColor } from '../theme/LoginTheme'
 interface Props extends StackScreenProps<ProductStackParams, 'SingleProductScreen'> { }
 
 const SingleProductScreen = ({ navigation, route }: Props) => {
-  const { name = '', id = '' } = route.params;
+  const { id = '', name = '',} = route.params;
   const { categories } = useCategories();
   const { loadProductById } = useContext(ProductsContext)
   const { _id, nombre, setFormValue, categoriaId, img, form, onChange } = useForm({
@@ -21,25 +21,22 @@ const SingleProductScreen = ({ navigation, route }: Props) => {
     nombre: name,
     img: ''
   })
-  let isMounted = true;
   
   useEffect(() => {
     navigation.setOptions({
-      title: name ? name : 'New Product'
+      title: nombre ? nombre : 'New Product'
     })
-  }, [name])
+  }, [nombre])
 
   useEffect(() => {
-    if (isMounted) 
-      loadProduct();
-  }, [])
+    loadProduct();
+  }, [categoriaId])
   
 
 
   const loadProduct = async () => {
     if (id.length === 0) return;
     const product = await loadProductById(id);
-    // while(product == null);
     setFormValue({
       _id: id,
       categoriaId: product.categoria._id,
@@ -68,12 +65,12 @@ const SingleProductScreen = ({ navigation, route }: Props) => {
         />
         <Picker
           selectedValue={categoriaId}
-          onValueChange={value => onChange(value, 'categoriaId')
+          onValueChange={(value) => onChange(value, 'categoriaId')
           }
         >
           {
-            categories && categories.map(category => (
-              <Picker.Item label={category.nombre} value={category._id} key={category._id} />
+            categories && categories.map(c => (
+              <Picker.Item label={c.nombre} value={c._id} key={c._id} />
             ))
           }
 
@@ -89,7 +86,7 @@ const SingleProductScreen = ({ navigation, route }: Props) => {
 
             func={saveOrUpdate}
             title="Save Product" />
-          { id.length > 0 && (
+          { _id.length > 0 && (
             <>
               <GreenButton
                 func={() => console.log("Camera")}
